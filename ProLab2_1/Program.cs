684fdaacb95072c11b12a0dbc5b0aff5fa50f220
Yunus Hanifi <oyunushanifi@gmail.com>
2024-02-28 17:46:02 +0300

    Start

--- ProLab2_1/Program.cs
+++ ProLab2_1/Program.cs
@@ -10,9 +10,8 @@ namespace ProLab2_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Test");
-            Console.ReadLine();
-            //bu birleşmiş
+            
+            
 
         }
     }
