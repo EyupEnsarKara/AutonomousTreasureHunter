27f1ff35939aff3c554b992b35feff8a130168ce
EyupEnsar <118618895+EyupEnsarKara@users.noreply.github.com>
2024-02-28 20:16:52 +0300

    .

--- ProLab2_1/Classes/Map.cs
+++ ProLab2_1/Classes/Map.cs
@@ -3,17 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace ProLab2_1.Classes
-{
+{ 
+    //add quad class
     internal class Map
-    {
+    { 
+       
+         
 
 
+       
 
-
-
-
+        
+ 
 
 
     }
